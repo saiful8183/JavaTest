1b1d9944e5aacfcb3735724e560f9543dbf7289f
saiful8183 <Manha103019@gmail.com>
2023-03-27 22:21:10 -0400

    javaTest

---
+++ Java test/src/java1/java.java
@@ -0,0 +1,14 @@
+package java1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class java {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Saiful Chowdhury\\OneDrive\\Desktop\\Automation\\chromedriver.exe");	
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
